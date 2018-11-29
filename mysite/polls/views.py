000586efb6fc799a645cc6c03e708c1c2fc2cf6e
Pavan Kumar <Pavan@testpress.in>
2018-11-29 16:03:37 +0530

    Django tutorial part 5 features added

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,7 @@ from .models import Question,Choice
 from django.urls import reverse
 from django.shortcuts import get_object_or_404, render
 from django.views import generic
+from django.utils import timezone
 
 
 class IndexView(generic.ListView):
@@ -10,8 +11,8 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """Return the last five published questions (not including those set to be pubished in future)."""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
